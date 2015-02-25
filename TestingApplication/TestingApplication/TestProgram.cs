b83820ad82cb66ce3535f844b7f3a5673f521758
Justin Jerrard <GitJerrard@users.noreply.github.com>
2015-02-25 10:31:42 +1000

    Added Console Application

---
+++ TestingApplication/TestingApplication/TestProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World\nPress any key to close...");
+
+            Console.ReadKey();
+        }
+    }
+}
